fe6b5acdc9d56b0711cd54f0c358b1ef8303c65f
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2019-11-27 17:43:00 +0400

    Issue and Teammate reference

--- src/main/kotlin/org/jetbrains/uberinjector/IssueCodeInjector.kt
+++ src/main/kotlin/org/jetbrains/uberinjector/IssueCodeInjector.kt
@@ -1,17 +1,32 @@
 package org.jetbrains.uberinjector
 
+import com.intellij.openapi.paths.WebReference
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiReference
 import com.intellij.psi.injection.ReferenceInjector
 import com.intellij.util.ProcessingContext
+import org.jetbrains.uast.UExpression
+import org.jetbrains.uast.evaluateString
+import org.jetbrains.uast.toUElement
+import javax.swing.Icon
 
 class IssueCodeInjector : ReferenceInjector() {
     override fun getId(): String = "uber-issue-code"
 
     override fun getDisplayName(): String = "Issue Code"
 
+    override fun getIcon(): Icon = UberInjectorIcons.LINK
+
     override fun getReferences(element: PsiElement, context: ProcessingContext, range: TextRange): Array<PsiReference> {
-        return emptyArray()
+        val expr = element.toUElement() as? UExpression ?: return emptyArray()
+        val value = expr.evaluateString() ?: return emptyArray()
+        if (value.isBlank()) return emptyArray()
+
+        val template = "https://youtrack.jetbrains.com/issue/"
+
+        val url = if (template.contains("{}")) template.replace("{}", value) else template + value
+
+        return arrayOf(WebReference(element, url))
     }
 }
--- src/main/kotlin/org/jetbrains/uberinjector/TeammateInjector.kt
+++ src/main/kotlin/org/jetbrains/uberinjector/TeammateInjector.kt
@@ -1,10 +1,24 @@
 package org.jetbrains.uberinjector
 
+import com.intellij.codeInsight.daemon.EmptyResolveMessageProvider
+import com.intellij.codeInspection.LocalQuickFix
+import com.intellij.codeInspection.LocalQuickFixProvider
+import com.intellij.find.actions.ShowUsagesAction
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.project.DumbService
+import com.intellij.openapi.ui.popup.JBPopupFactory
 import com.intellij.openapi.util.TextRange
+import com.intellij.psi.ElementManipulators
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiReference
+import com.intellij.psi.PsiReferenceBase
+import com.intellij.psi.impl.FakePsiElement
 import com.intellij.psi.injection.ReferenceInjector
+import com.intellij.psi.meta.PsiMetaData
+import com.intellij.psi.meta.PsiMetaOwner
+import com.intellij.psi.meta.PsiPresentableMetaData
 import com.intellij.util.ProcessingContext
+import javax.swing.Icon
 
 class TeammateInjector : ReferenceInjector() {
     override fun getId(): String = "uber-teammate"
@@ -12,6 +26,93 @@ class TeammateInjector : ReferenceInjector() {
     override fun getDisplayName(): String = "Teammate Username"
 
     override fun getReferences(element: PsiElement, context: ProcessingContext, range: TextRange): Array<PsiReference> {
-        return emptyArray()
+        return arrayOf(TeammateReference(element))
+    }
+
+    class TeammateReference(element: PsiElement) :
+        PsiReferenceBase<PsiElement>(element, ElementManipulators.getValueTextRange(element)),
+        EmptyResolveMessageProvider, LocalQuickFixProvider {
+
+        private fun getReferenceTypeName(): String = "Teammate"
+
+        private fun validate(value: String): Boolean = value.isNotBlank()
+
+        override fun resolve(): PsiElement? {
+            val value = value
+            if (!validate(value)) return null
+
+            return TeammatePsiElement(element, getReferenceTypeName())
+        }
+
+        override fun getUnresolvedMessagePattern(): String {
+            return "Incorrect ${getReferenceTypeName()} ''{0}''"
+        }
+
+        override fun getQuickFixes(): Array<LocalQuickFix>? {
+            return emptyArray()
+        }
+
+        override fun isReferenceTo(element: PsiElement): Boolean {
+            return element is TeammatePsiElement
+        }
+
+        override fun getVariants(): Array<Any> {
+            return emptyArray()
+        }
+    }
+
+    class TeammatePsiElement(
+        private val parent: PsiElement,
+        private val typeName: String
+    ) : FakePsiElement(), PsiMetaOwner, PsiPresentableMetaData {
+
+        override fun getName(): String {
+            return ElementManipulators.getValueText(parent)
+        }
+
+        override fun getParent(): PsiElement {
+            return parent
+        }
+
+        override fun getName(context: PsiElement): String? {
+            return name
+        }
+
+        override fun getIcon(): Icon? {
+            return null
+        }
+
+        override fun getDeclaration(): PsiElement {
+            return this
+        }
+
+        override fun init(element: PsiElement) {}
+
+        override fun getMetaData(): PsiMetaData? {
+            return this
+        }
+
+        override fun getTypeName(): String? {
+            return typeName
+        }
+
+        override fun getNavigationElement(): PsiElement {
+            return this
+        }
+
+        override fun canNavigate(): Boolean {
+            return true
+        }
+
+        override fun navigate(requestFocus: Boolean) {
+            if (DumbService.getInstance(project).isDumb) return
+            val editor = FileEditorManager.getInstance(project).selectedTextEditor ?: return
+            val popupPosition = JBPopupFactory.getInstance().guessBestPopupLocation(editor)
+
+            ShowUsagesAction().startFindUsages(
+                this, popupPosition, editor,
+                ShowUsagesAction.getUsagesPageSize()
+            )
+        }
     }
 }
